79660c3787c0522b0b4a536ec70905ef93b92fd2
Svvay <svvay@ukr.net>
2019-12-27 23:38:38 +0200

    27.12

---
+++ work_directory/BinaryAddition.py
@@ -0,0 +1,11 @@
+# Implement a function that adds two numbers together and returns their sum in binary.
+# The conversion can be done before, or after the addition.
+# The binary number returned should be a string.
+
+# a = 2
+# b = 2
+
+def add_binary(a,b):
+        return bin(a + b)[2:]
+# print(add_binary(a, b))
+
